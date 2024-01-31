17cb5f1926a0074608be8ae66ec377c8f3f20aa4
surfineer <123465523+surfineer@users.noreply.github.com>
2024-01-31 11:03:28 +0100

    init DOM cheatsheets

---
+++ DOM/script.js
@@ -0,0 +1,9 @@
+
+
+// PARENTS & CHILDREN
+
+let parentElement = document.getElementById('habitat').parentNode;
+console.log(parentElement);
+let childElements = document.getElementById('habitat').children;
+console.log(childElements);
+
